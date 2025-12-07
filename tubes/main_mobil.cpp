5938e7f0fb0474a1904ed9ba5214354e63fdc4cb
Pandora91105 <zidanenairda@gmail.com>
2025-12-07 23:20:31 +0700

    Update main_mobil.cpp

---
+++ tubes/main_mobil.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include "mobil.h"
+#include "sales.h"
+#include "string"
+
+using namespace std;
+
+void createListMobil(listMobil &L){
+    L.first = nullptr;
+    L.last = nullptr;
+}
+
+adrMobil createElmMobil(listMobil &L, string merk, string warna, int stok ){
+    adrMobil p = new elmMobil;
+    p->info.Merk = merk;
+    p->info.warna = warna;
+    p->info.stock = stok;
+    p->next = nullptr;
+    p->prev = nullptr;
+    p->firstSales = nullptr;
+
+    return p;
+}
+
+void insertFirstMobil (listMobil &L, adrMobil p){
+    if(L.first == nullptr  && L.last == nullptr){
+        L.first = p;
+        L.last = p;
+    }else{
+        p->next = L.first;
+        L.first->prev = p;
+        L.first = p;
+    }
+}
+
+void insertLastMobil(listMobil &L, adrMobil p){
+    if(L.first == nullptr  && L.last == nullptr){
+        L.first = p;
+        L.last = p;
+    }else{
+        p->prev = L.last;
+        L.last->next = p;
+        L.last = p;
+    }
+}
+
+void insertAfterMobil(listMobil &L, adrMobil p, adrMobil prec){
+    if (L.first == nullptr && L.last == nullptr){
+        L.first = p;
+        L.last = p;
+    }else if (L.first == L.last){
+        p->prev = L.last;
+        L.last->next = p;
+        L.last = p;
+    }else {
+        p->next = prec->next;
+        p->prev = prec;
+        prec->next->prev = p;
+        prec->next = p;
+    }
+}
+
+void deleteFirstMobil(listMobil L, adrMobil p){
+    if (L.first == nullptr && L.last == nullptr){
+        cout << "List Kosong" << endl;
+    }else if(L.first == L.last){
+        p = L.first;
+        L.first = nullptr;
+        L.last = nullptr;
+    }else{
+        p = L.first;
+        L.first = p->next;
+        p->next = nullptr;
+    }
+}
+
+void deleteLastMobil(listMobil L, adrMobil p){
+    if (L.first == nullptr && L.last == nullptr){
+        cout << "List Kosong" << endl;
+    }else if(L.first == L.last){
+        p = L.first;
+        L.first = nullptr;
+        L.last = nullptr;
+    }else{
+        p = L.last;
+        L.last = p->prev;
+        p->prev = nullptr;
+    }
+}
+
+void deleteAfterMobil(listMobil L, adrMobil p, adrMobil prec){
+    if (L.first == nullptr && L.last == nullptr){
+        cout << "List Kosong" << endl;
+    }else if(L.first == L.last){
+        p = L.first;
+        L.first = nullptr;
+        L.last = nullptr;
+    }else{
+        p = prec->next;
+        prec->next = p->next;
+        p->next->prev = prec;
+        p->next = nullptr;
+        p->prev = nullptr;
+    }
+}
+
+adrMobil findElmMobil(listMobil L, string x){
+    adrMobil p = L.first;
+    while (p->next != nullptr && p->info.Merk != x ){
+        p = p->next;
+    }
+    return p;
+}
+
+void viewMobil(listMobil L){
+    adrMobil p = L.first;
+    while(p != nullptr){
+        cout << p ->info.Merk << endl;
+        cout << p ->info.stock << endl;
+        cout << p ->info.warna << endl;
+        p = p ->next;
+    }
+
+}
