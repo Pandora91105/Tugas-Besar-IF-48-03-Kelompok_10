c0f5024edde60f4e9ecb14d181a597d416fcfc1c
Pandora91105 <zidanenairda@gmail.com>
2025-12-07 23:20:09 +0700

    Update mobil.h

--- tubes/mobil.h
+++ tubes/mobil.h
@@ -1,6 +1,28 @@
 #ifndef MOBIL_H_INCLUDED
 #define MOBIL_H_INCLUDED
+#include <iostream>
+#include "string"
+#include "sales.h"
+using namespace std;
 
+
+typedef struct elmMobil *adrMobil;
+
+struct mobil {
+    string warna;
+    string Merk;
+    int stock;
+};
+
+struct elmMobil {
+    mobil info;
+    adrMobil next, prev;
+    adrSales firstSales;
+};
+
+struct listMobil {
+    adrMobil first, last;
+};
 
 
 #endif // MOBIL_H_INCLUDED
