a0afcfc7ec196821117112ec26067e80bbb71e4c
Pandora91105 <zidanenairda@gmail.com>
2025-12-07 23:19:49 +0700

    Add sales structures and type definitions

--- tubes/sales.h
+++ tubes/sales.h
@@ -1,6 +1,23 @@
 #ifndef SALES_H_INCLUDED
 #define SALES_H_INCLUDED
+#include <iostream>
+#include "string"
 
+using namespace std;
 
+typedef struct elmSales *adrSales;
 
+struct sales {
+    string Nama;
+    int penjualan;
+    string kode;
+};
+
+struct elmSales {
+    sales info;
+    adrSales next;
+};
+ struct listSales{
+    adrSales first;
+ };
 #endif // SALES_H_INCLUDED
