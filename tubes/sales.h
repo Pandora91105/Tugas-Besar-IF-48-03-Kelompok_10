3b6752330d0ecf5193c5ecd2327dcaa226ca2be8
Pandora91105 <zidanenairda@gmail.com>
2025-12-19 23:03:00 +0700

    Add sales list management functions in sales.h

--- tubes/sales.h
+++ tubes/sales.h
@@ -6,6 +6,8 @@
 using namespace std;
 
 typedef struct elmSales *adrSales;
+typedef struct elmMobil *adrMobil;
+struct listMobil;
 
 struct sales {
     string Nama;
@@ -20,4 +22,16 @@ struct elmSales {
  struct listSales{
     adrSales first;
  };
+
+void createListSales(listSales &L);
+adrSales createElmSales( string nama, string kode, int penjualan);
+void insertFirstSales(adrMobil &p, adrSales q);
+void insertLastSales(adrMobil &p, adrSales q);
+void insertAfterSales(adrMobil &p, adrSales q, adrSales prec);
+void deleteFirstSales(adrMobil &p, adrSales q);
+void deleteLastSales(adrMobil &p, adrSales q);
+void deleteAfterSales(adrMobil &p, adrSales q, adrSales prec);
+adrSales findSales(adrMobil p, string kode);
+void viewSales(listMobil L);
+void performaSales(listSales sales, listMobil mobil, string kode);
 #endif // SALES_H_INCLUDED
